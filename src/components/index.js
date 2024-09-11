c4410ba661fb7269af282013e1f748f5a4e7c38e
VirajK04 <virajkhairnar4@gmail.com>
2024-09-11 23:07:40 +0530

    Complete Project

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Button from './Button'
+import Input from './Input'
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Select from "./Select";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Button,
+    Input,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Select
+}
