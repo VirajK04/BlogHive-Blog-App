3c2384e13126406ff710791f1848d36fea49a74f
VirajK04 <virajkhairnar4@gmail.com>
2024-09-12 00:34:50 +0530

    added T iny mce key

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -16,7 +16,7 @@ function PostForm({post}) {
   })
 
   const navigate = useNavigate()
-  const userData = useSelector(state => state.user.userData)
+  const userData = useSelector(state => state.auth.userData)
 
   const submit = async (data) => {
     if (post) {
@@ -53,7 +53,7 @@ function PostForm({post}) {
     if(value && typeof value === 'string'){
       return value.trim()
       .toLowerCase()
-      .replace(/^[a-zA-Z\d\s]+/g, '-')
+      .replace(/[^a-zA-Z\d\s]+/g, '-')
       .replace(/\s/g, "-")
     }
     return ''
